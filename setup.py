329dd5b8e166fd57e149f6c147e3e0da47f5f55a
davidtnfsh <davidtnfsh@gmail.com>
2017-01-22 22:25:29 +0800

    add another dir to make pip register success

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name = 'campasscrawler',
-    packages = ['campasscrawler'],
+    name = 'CampassCrawler',
+    packages = ['CampassCrawler'],
     version = '1.0',
     description = 'Collections of Crawlers for Taiwan Universities.',
     author = 'davidtnfsh',
     author_email = 'davidtnfsh@gmail.com',
-    url = 'https://github.com/Stufinite/campasscrawler',
-    download_url = 'https://github.com/Stufinite/campasscrawler/archive/v1.0.tar.gz',
+    url = 'https://github.com/Stufinite/CampassCrawler',
+    download_url = 'https://github.com/Stufinite/CampassCrawler/archive/v1.0.tar.gz',
     keywords = ['Crawler', 'campass'],
     classifiers = [],
     license='',
