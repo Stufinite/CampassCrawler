7439422a312b54880aa5c392ba9bcaa0977253dd
davidtnfsh <davidtnfsh@gmail.com>
2017-01-23 22:55:26 +0800

    forgot to claim what license it use

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from distutils.core import setup
 setup(
     name = 'CampassCrawler',
     packages = ['CampassCrawler'],
-    version = '1.2',
+    version = '1.3',
     description = 'Collections of Crawlers for Taiwan Universities.',
     author = 'davidtnfsh',
     author_email = 'davidtnfsh@gmail.com',
     url = 'https://github.com/Stufinite/CampassCrawler',
-    download_url = 'https://github.com/Stufinite/CampassCrawler/archive/v1.2.tar.gz',
+    download_url = 'https://github.com/Stufinite/CampassCrawler/archive/v1.3.tar.gz',
     keywords = ['Crawler', 'campass'],
     classifiers = [],
-    license='',
+    license='GNU GPLv3',
     install_requires=[
         'requests',
         'simplejson',
