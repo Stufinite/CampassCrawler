21767afe51f361526d2a71841c22280e82c468bc
david <davidtnfsh@gmail.com>
2017-07-17 16:52:39 +0800

    finish schema + only crawl bachelor

--- crawler-NSYSU-course/NSYSU.py
+++ crawler-NSYSU-course/NSYSU.py
@@ -5,12 +5,22 @@ res = requests.get('http://selcrs.nsysu.edu.tw/menu1/qrycourse.asp')
 res.encoding = 'big5'
 soup = BeautifulSoup(res.text, "html.parser")
 
-dept_table = {i.text:i['value'] for i in soup.select('#DPT_ID select')[0] if i.text != ''}
+dept_table = {i.text:i['value'] for i in soup.select('#DPT_ID select')[0] if i.text != '' and (i['value'].startswith('A') or i['value'].startswith('B'))}
 latest_semester = soup.find('select', {'name':'D0'}).select('option')[0]['value']
 
-result = []
-for i in pyprind.prog_bar(dept_table.values()):
-	res = requests.get("http://selcrs.nsysu.edu.tw/menu1/dplycourse.asp?a=1&D0={semester}&DEG_COD={degree}&D1={deptcode}&HIS=1&TYP=1&bottom_per_page=10&data_per_page=20".format(semester=latest_semester, degree=i[0], deptcode=i))
+day_table = {
+	'一':1,
+	'二':2,
+	'三':3,
+	'四':4,
+	'五':5,
+	'六':6,
+	'日':7,
+}
+
+final = []
+for key, value in pyprind.prog_bar(dept_table.items()):
+	res = requests.get("http://selcrs.nsysu.edu.tw/menu1/dplycourse.asp?a=1&D0={semester}&DEG_COD={degree}&D1={deptcode}&HIS=1&TYP=1&bottom_per_page=10&data_per_page=20".format(semester=latest_semester, degree=value[0], deptcode=value))
 	res.encoding = 'big5'	
 	soup = BeautifulSoup(res.text, "html.parser")
 
@@ -18,12 +28,27 @@ for i in pyprind.prog_bar(dept_table.values()):
 	for page in range(1, last_page+1):
 
 		driver = webdriver.PhantomJS(executable_path='../phantomjs')
-		driver.get("http://selcrs.nsysu.edu.tw/menu1/dplycourse.asp?a=1&D0={semester}&DEG_COD={degree}&D1={deptcode}&HIS=1&TYP=1&bottom_per_page=10&data_per_page=20&page={page}".format(semester=latest_semester, degree=i[0], deptcode=i, page=str(page)))
+		driver.get("http://selcrs.nsysu.edu.tw/menu1/dplycourse.asp?a=1&D0={semester}&DEG_COD={degree}&D1={deptcode}&HIS=1&TYP=1&bottom_per_page=10&data_per_page=20&page={page}".format(semester=latest_semester, degree=value[0], deptcode=value, page=str(page)))
 		soup = BeautifulSoup(driver.page_source, "html.parser")
 
 		schema, weekdays, course = [i.text for i in soup.select('tr')[1].select('td')], [i.text for i in soup.select('tr')[2].select('td')], soup.select('tr')[3:-2]
 		schema = schema[:-2] + weekdays + schema[-1:]
-		result += (dict(zip(schema, (j.text for j in i.select('td')[1:]))) for i in course)
+		for courseDict in (dict(zip(schema, (j.text for j in i.select('td')[1:]))) for i in course):
+			result = {
+				'department': key,
+				'for_dept':courseDict['系所別'],
+				'class':courseDict['年級'],
+				'title_parsed':courseDict['科目名稱'],
+				'time':[{'day':day_table[time], 'time':list(courseDict[time])} for time in courseDict if time in day_table and courseDict[time]!='\xa0'],
+				'credits':float(courseDict['學分']),
+				'obligatory_tf':True if courseDict['必選修'] == '必' else False,
+				'professor':courseDict['授課教師'],
+				'location':courseDict['教室'],
+				'code':courseDict['課號'],
+				'note':courseDict['備註'],
+			}
+
+			final.append(result)
 
 
-json.dump(result, open('nsysu.json', 'w'))
+json.dump(final, open('nsysu.json', 'w'))
