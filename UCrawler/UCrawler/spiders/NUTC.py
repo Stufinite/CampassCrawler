6a22cb282ae7d4c209207c9efe81069e5b169427
jeremy4555 <jeremy4555@yahoo.com.tw>
2017-08-03 10:52:48 +0800

    updateNUTCInPhantomjs

--- UCrawler/UCrawler/spiders/NUTC.py
+++ UCrawler/UCrawler/spiders/NUTC.py
@@ -28,7 +28,6 @@ class NutcSpider(scrapy.Spider):
         json.dump(genra, f)
 
     def start_requests(self):
-        #driver = cross_selenium(True)
         driver = cross_selenium()
         driver.get(self.start_urls[0])
         dropdown = driver.find_element_by_id('sem')
