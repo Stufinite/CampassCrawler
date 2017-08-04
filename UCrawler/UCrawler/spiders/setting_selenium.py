d4699a8085bd0170e2dca7ef0499a12c5998c8a2
jeremy4555 <jeremy4555@yahoo.com.tw>
2017-08-04 11:06:07 +0800

    fix phantomjs driver

--- UCrawler/UCrawler/spiders/setting_selenium.py
+++ UCrawler/UCrawler/spiders/setting_selenium.py
@@ -13,7 +13,8 @@ def cross_selenium(chrome=False):
 		if chrome:
 			driver = webdriver.Chrome(executable_path=os.path.join('.', 'chromedriver'))
 		else:
-			driver = webdriver.PhantomJS(executable_path=os.path.join('.', 'phantomjs'))
+			# driver = webdriver.PhantomJS(executable_path=os.path.join('.', 'phantomjs'))
+			driver = webdriver.PhantomJS()
 
 	return driver 
 
