fe05747883b97495fd619458143922e2de00d811
SongSong <qpoome@gmail.com>
2017-08-13 15:44:26 +0800

    修正相關BUG
    修正相關BUG 並使爬出來的MCU.json 可以匯入DB

--- UCrawler/UCrawler/spiders/MCU.py
+++ UCrawler/UCrawler/spiders/MCU.py
@@ -1,13 +1,14 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import scrapy, re, json, urllib
+import scrapy
+import re
+import json
 from scrapy.http import FormRequest
+import urllib
 from UCrawler.items import UcrawlerItem
-from .setting_selenium import cross_selenium
-
 
 def innerHTML(element):
-    return element.decode_contents(formatter="html")
+        return element.decode_contents(formatter="html")
 class McuSpider(scrapy.Spider):
     name = 'MCU'
     start_urls =["http://www.mcu.edu.tw/student/new-query/sel-query/qslist.asp","http://web.gec.mcu.edu.tw/zh-hant/node/62"]
@@ -19,10 +20,11 @@ class McuSpider(scrapy.Spider):
     chineseKey ='研'
     def start_requests(self):
         #先用selenium模仿web使用者動作
-        driver=cross_selenium()
+        driver=webdriver.Chrome(executable_path="./chromedriver")
         driver.get(self.start_urls[0])
         #一些仿USER的動作
         #放入bs4裏面
+        #.decode('big5', 'ignore')
         soupsch = BeautifulSoup(driver.page_source, "html.parser")
         #取得所有系所列表
         deptRefNodeList = [ re.sub(r"\r|\n|\t|[ ]","",innerHTML(x)) for x in soupsch.find('select',{'name':'dept1'}).find_all('option')]
@@ -34,8 +36,6 @@ class McuSpider(scrapy.Spider):
         #取的所有的通式課程類別對照表
         driver.get(self.start_urls[1])
         disciplineSoup =BeautifulSoup(driver.page_source,"html.parser")
-        driver.close()
-
         disciplineRows = disciplineSoup.find('table',{'class':'responsive'}).find('tbody').find_all('tr')
         disciplineTitle =''
         disciplineDesc =''
@@ -64,6 +64,7 @@ class McuSpider(scrapy.Spider):
             else:
                 schbody ={'sch' : i }#urllib.parse.urlencode(schbody)
                 schheaders={'Content-type': 'application/x-www-form-urlencoded'}
+                #yield scrapy.http.FormRequest("http://www.mcu.edu.tw/student/new-query/sel-query/qslist_1.asp", callback=self.parse_detail, formdata=schbody)
                 yield scrapy.http.Request("http://www.mcu.edu.tw/student/new-query/sel-query/qslist_1.asp",method ="POST",body=urllib.parse.urlencode(schbody).encode('utf-8'),headers=schheaders,callback=self.parse)
     
     def parse(self,response):
@@ -84,10 +85,14 @@ class McuSpider(scrapy.Spider):
                 ClassDesc=IDClass[1]
             else:
                 ClassDesc=None
+            #ClassDesc=IDClass[0]
+            #innerHTML(col[2].find_all('font')[0]).replace('&nbsp;','').split(' ')[0]
             #抓取課程編號
+            #IDCode =innerHTML(col[1].find_all('font')[0]).split(' ')[0].replace('&nbsp;','')
             classdict['code']=innerHTML(col[1].find_all('font')[0]).split(' ')[0].replace('&nbsp;','')
             #學分
-            classdict['credits']=innerHTML(col[9].find_all('font')[0])
+            classdict['credits']=float(innerHTML(col[9].find_all('font')[0]))
+            #print(ID[0:2])
             
             #開課給哪個系
             for_dept =''
@@ -103,14 +108,15 @@ class McuSpider(scrapy.Spider):
                             for_dept =ClassDesc[0:ClassDesc.find(self.chineseNum[i])]
                             for_grade =ClassDesc[ClassDesc.find(self.chineseNum[i]):len(ClassDesc)]
             classdict['for_dept'] =for_dept
-            classdict['grade']= for_grade
+            classdict['grade']= innerHTML(col[6].find_all('font')[0]).replace('&nbsp;','').strip()
             #開課單位
             if self.deptRefNodeDict.get(IDClass[0][0:2]) is not None:
                 classdict['department']=self.deptRefNodeDict[IDClass[0][0:2]]
             else:
-                classdict['department']=None
+                classdict['department']=''
             #上課地點
             classdict['location']=innerHTML(col[7].find_all('font')[0]).replace('&nbsp;','').strip().split('<br/>')
+            #classdict['obligatory_tf'] =innerHTML(col[8].find_all('font')[0])
             #是否必修
             if innerHTML(col[8].find_all('font')[0]) == '必修' :
                 classdict['obligatory_tf'] = True
@@ -119,25 +125,45 @@ class McuSpider(scrapy.Spider):
             #切時間
             timeArr =[]
             timelist =innerHTML(col[5].find_all('font')[0]).replace('&nbsp;','').split('<br/>')
+                #print(range(len(timelist)))
             for i in range(len(timelist)):
                     #最後一航不用看
                 if i!=len(timelist)-1:
                     tmp =timelist[i].split(':')
                     timeDict ={}
-                    timeDict['day']=tmp[0].replace('星期','').strip()
+                    if int(tmp[0].replace('星期','').strip())!=0 :
+                        timeDict['day']=int(tmp[0].replace('星期','').strip())
+                    else :
+                        timeDict['day']=6
                     Sessions =tmp[1].replace('節','').strip().split(' ')
                     SessionArr =[]
                     for j in range(len(Sessions)):
                         if Sessions[j]!='':
-                            SessionArr.append(int(Sessions[j]))
+                            if Sessions[j] == '40':
+                                SessionArr.append(10)
+                            elif Sessions[j] == '20':
+                                print('中午不考慮喔')
+                            elif Sessions[j] == '50':
+                                SessionArr.append(11)
+                            elif Sessions[j] == '60':
+                                SessionArr.append(12)
+                            elif Sessions[j] == '70':
+                                SessionArr.append(13)
+                            else:
+                                SessionArr.append(int(Sessions[j]))
+                        
                     timeDict['time']=SessionArr
                     timeArr.append(timeDict)
+                #else :
+                    #print(i)
             classdict['time']=timeArr
             classdict['title']=innerHTML(col[1].find_all('font')[0]).split(' ')[1].replace('&nbsp;','')
-            classdict['discipline'] =None
+            classdict['category'] =innerHTML(col[8].find_all('font')[0])+'類'
+            classdict['discipline'] =''
             for disciplineInfo in self.disciplineList :
                 if disciplineInfo['subject'] == classdict['title'] :
                     classdict['discipline']=disciplineInfo['discipline']
+                    #print(classdict['discipline'])
                     break
             classdict['note']=innerHTML(col[13].find_all('font')[0]).replace('&nbsp;','').strip()
             classdict['professor']=innerHTML(col[4].find_all('a')[0].find_all('font')[0]).replace('&nbsp;','').replace('<br/>','、')
@@ -146,4 +172,5 @@ class McuSpider(scrapy.Spider):
                 classdict['professor'] =''
             #開課學校
             classdict['campus']='MCU'#+classdict['campus'][classdict['campus'].find('【'):len(classdict['campus'])]
+            #print(classdict['discipline'])
             yield classdict
